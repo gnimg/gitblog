0e5ebf36b95b50b60f76390ea2214eafb7699d02
gnimg <95269189+gnimg@users.noreply.github.com>
2021-12-03 14:40:09 +0800

    Frend list check before added to readme.

--- main.py
+++ main.py
@@ -145,17 +145,20 @@ def add_md_top(repo, md, me):
 def add_md_firends(repo, md, me):
     s = FRIENDS_TABLE_HEAD
     friends_issues = list(repo.get_issues(labels=FRIENDS_LABELS))
-    for issue in friends_issues:
-        for comment in issue.get_comments():
-            if is_hearted_by_me(comment, me):
-                try:
-                    s += _make_friend_table_string(comment.body)
-                except Exception as e:
-                    print(str(e))
-                    pass
-    with open(md, "a+", encoding="utf-8") as md:
-        md.write("## 友情链接\n")
-        md.write(s)
+    if len(friends_issues) > 0:
+        for issue in friends_issues:
+            for comment in issue.get_comments():
+                if is_hearted_by_me(comment, me):
+                    try:
+                        s += _make_friend_table_string(comment.body)
+                    except Exception as e:
+                        print(str(e))
+                        pass
+        with open(md, "a+", encoding="utf-8") as md:
+            md.write("## 友情链接\n")
+            md.write(s)
+    else:
+        pass
 
 
 def add_md_recent(repo, md, me):
